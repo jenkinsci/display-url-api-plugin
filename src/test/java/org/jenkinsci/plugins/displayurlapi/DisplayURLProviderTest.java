04f14c5da49e4d9576760e386551ad051d2129fc
James Dumay <jameswdumay@gmail.com>
2016-09-01 10:46:35 +1000

    Cleanup test

--- src/test/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProviderTest.java
+++ src/test/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProviderTest.java
@@ -19,21 +19,17 @@ public class DisplayURLProviderTest {
 
     @Rule
     public JenkinsRuleWithLocalPort rule = new JenkinsRuleWithLocalPort();
-    private Run<?, ?> run;
-
-    private Job<?, ?> job;
 
     @Test
     public void urls() throws Exception {
         MockFolder folder = rule.createFolder("my folder");
         FreeStyleProject project = (FreeStyleProject) folder.createProject(FreeStyleProject.DESCRIPTOR, "my job", false);
-        job = project;
-        run = project.scheduleBuild2(0).get();
+        Run<?, ?> run = project.scheduleBuild2(0).get();
 
         String root = DisplayURLProvider.get().getRoot();
         assertEquals("http://localhost:" + rule.getLocalPort() + "/jenkins/", root);
         assertEquals(root + "job/my%20folder/job/my%20job/1/", DisplayURLProvider.get().getRunURL(run));
-        assertEquals(root + "job/my%20folder/job/my%20job/", DisplayURLProvider.get().getJobURL(job));
+        assertEquals(root + "job/my%20folder/job/my%20job/", DisplayURLProvider.get().getJobURL(project));
         assertEquals(root + "job/my%20folder/job/my%20job/changes", DisplayURLProvider.get().getChangesURL(run));
     }
 
@@ -43,7 +39,7 @@ public class DisplayURLProviderTest {
 
         MockFolder folder = rule.createFolder("my folder");
         FreeStyleProject project = (FreeStyleProject) folder.createProject(FreeStyleProject.DESCRIPTOR, "my job", false);
-        FreeStyleBuild run = project.scheduleBuild2(0).get();
+        Run<?, ?> run = project.scheduleBuild2(0).get();
 
         AbstractTestResultAction action = mock(AbstractTestResultAction.class);
         when(action.getUrlName()).thenReturn("action");
