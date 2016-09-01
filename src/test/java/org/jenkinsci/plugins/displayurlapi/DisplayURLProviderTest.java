757cd1a4e40a62556d76937ba4c4de531919746c
James Dumay <jameswdumay@gmail.com>
2016-09-01 10:14:45 +1000

    add test for getTestURL

--- src/test/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProviderTest.java
+++ src/test/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProviderTest.java
@@ -1,14 +1,19 @@
 package org.jenkinsci.plugins.displayurlapi;
 
+import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Job;
 import hudson.model.Run;
+import hudson.tasks.test.AbstractTestResultAction;
+import hudson.tasks.test.TestResult;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.MockFolder;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DisplayURLProviderTest {
 
@@ -30,6 +35,25 @@ public class DisplayURLProviderTest {
         assertEquals(root + "job/my%20folder/job/my%20job/1/", DisplayURLProvider.get().getRunURL(run));
         assertEquals(root + "job/my%20folder/job/my%20job/", DisplayURLProvider.get().getJobURL(job));
         assertEquals(root + "job/my%20folder/job/my%20job/changes", DisplayURLProvider.get().getChangesURL(run));
+    }
+
+    @Test
+    public void testGetTestURL() throws Exception {
+        TestResult result = mock(TestResult.class);
+
+        MockFolder folder = rule.createFolder("my folder");
+        FreeStyleProject project = (FreeStyleProject) folder.createProject(FreeStyleProject.DESCRIPTOR, "my job", false);
+        FreeStyleBuild run = project.scheduleBuild2(0).get();
+
+        AbstractTestResultAction action = mock(AbstractTestResultAction.class);
+        when(action.getUrlName()).thenReturn("action");
+
+        when(result.getRun()).thenReturn((Run)run);
+        when(result.getTestResultAction()).thenReturn(action);
+        when(result.getUrl()).thenReturn("/some id with spaces");
+
+        String testUrl = DisplayURLProvider.get().getTestUrl(result);
+        assertEquals("http://localhost:" + rule.getLocalPort() + "/jenkins/job/my%20folder/job/my%20job/1/action/some%20id%20with%20spaces", testUrl);
     }
 
     class JenkinsRuleWithLocalPort extends JenkinsRule {
