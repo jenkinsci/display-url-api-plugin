d25900536fb87e239c658f2576866d08ce85436d
Olivier Lamy <oliver.lamy@gmail.com>
2020-11-18 11:05:19 +1000

    [JENKINS-64119] Display URL Plugin does not use user prefered implementations (#42)
    * [JENKINS-64119] Display URL Plugin does not use user prefered implementations
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * Update src/main/java/org/jenkinsci/plugins/displayurlapi/actions/AbstractDisplayAction.java
    
    Co-authored-by: Carroll Chiou <cchiou@cloudbees.com>
    
    * use ACL to switch current user
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    Co-authored-by: Carroll Chiou <cchiou@cloudbees.com>

--- src/test/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProviderTest.java
+++ src/test/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProviderTest.java
@@ -2,16 +2,22 @@ package org.jenkinsci.plugins.displayurlapi;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.EnvVars;
+import hudson.ExtensionList;
 import hudson.PluginWrapper;
 import hudson.model.FreeStyleProject;
+import hudson.model.Job;
 import hudson.model.Run;
-import java.util.HashMap;
-import java.util.Map;
+import hudson.model.User;
+import hudson.security.ACL;
 import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.displayurlapi.user.PreferredProviderUserProperty;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.MockFolder;
 import org.jvnet.hudson.test.TestExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -48,6 +54,162 @@ public class DisplayURLProviderTest {
         assertEquals(DisplayURLProvider.get().getJobURL(project), environment.get("JOB_DISPLAY_URL"));
     }
 
+    @TestExtension
+    public static class TestSysPropDisplayURLProvider extends DisplayURLProvider
+    {
+
+        public static final String EXTRA_CONTENT_IN_URL = "syspropsdefinedprovider";
+
+        private DisplayURLProvider getRootProvider()
+        {
+            return ExtensionList.lookup( DisplayURLProvider.class ).getDynamic(
+                ClassicDisplayURLProvider.class.getName() );
+        }
+
+        @Override
+        public String getRunURL( Run<?, ?> run )
+        {
+            return getRootProvider().getRunURL( run ) + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getArtifactsURL( Run<?, ?> run )
+        {
+            return getRootProvider().getArtifactsURL( run ) + "/" + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getChangesURL( Run<?, ?> run )
+        {
+            return getRootProvider().getChangesURL( run ) + "/" + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getTestsURL( Run<?, ?> run )
+        {
+            return getRootProvider().getTestsURL( run ) + "/" + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getJobURL( Job<?, ?> project )
+        {
+            return getRootProvider().getJobURL( project ) + EXTRA_CONTENT_IN_URL;
+        }
+    }
+
+    @Test
+    public void urlsWithSysPropProvider() throws Exception {
+        System.setProperty("jenkins.displayurl.provider",
+                           TestSysPropDisplayURLProvider.class.getName());
+        MockFolder folder = rule.createFolder("my folder");
+        FreeStyleProject project = (FreeStyleProject) folder
+            .createProject(rule.jenkins.getDescriptorByType(FreeStyleProject.DescriptorImpl.class), "my job",
+                           false);
+        Run<?, ?> run = project.scheduleBuild2(0).get();
+
+        String root = DisplayURLProvider.get().getRoot();
+        assertEquals("http://localhost:" + rule.getLocalPort() + "/jenkins/", root);
+        assertEquals(root + "job/my%20folder/job/my%20job/1/" + TestSysPropDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getRunURL(run));
+        assertEquals(root + "job/my%20folder/job/my%20job/" + TestSysPropDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getJobURL(project));
+        assertEquals(root + "job/my%20folder/job/my%20job/1/artifact/" + TestSysPropDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getArtifactsURL(run));
+        assertEquals(root + "job/my%20folder/job/my%20job/changes/" + TestSysPropDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getChangesURL(run));
+        assertEquals(root + "job/my%20folder/job/my%20job/1/testReport/" + TestSysPropDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getTestsURL(run));
+
+        EnvVars environment = run.getEnvironment();
+        assertEquals(DisplayURLProvider.get().getRunURL(run), environment.get("RUN_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getArtifactsURL(run), environment.get("RUN_ARTIFACTS_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getChangesURL(run), environment.get("RUN_CHANGES_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getTestsURL(run), environment.get("RUN_TESTS_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getJobURL(project), environment.get("JOB_DISPLAY_URL"));
+        System.setProperty("jenkins.displayurl.provider", "");
+    }
+
+    @TestExtension
+    public static class TestUserDisplayURLProvider extends DisplayURLProvider
+    {
+
+        public static final String EXTRA_CONTENT_IN_URL = "userdefinedprovider";
+
+        private DisplayURLProvider getRootProvider()
+        {
+            return ExtensionList.lookup( DisplayURLProvider.class ).getDynamic(
+                ClassicDisplayURLProvider.class.getName() );
+        }
+
+        @Override
+        public String getRunURL( Run<?, ?> run )
+        {
+            return getRootProvider().getRunURL( run ) + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getArtifactsURL( Run<?, ?> run )
+        {
+            return getRootProvider().getArtifactsURL( run ) + "/" + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getChangesURL( Run<?, ?> run )
+        {
+            return getRootProvider().getChangesURL( run ) + "/" + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getTestsURL( Run<?, ?> run )
+        {
+            return getRootProvider().getTestsURL( run ) + "/" + EXTRA_CONTENT_IN_URL;
+        }
+
+        @Override
+        public String getJobURL( Job<?, ?> project )
+        {
+            return getRootProvider().getJobURL( project ) + EXTRA_CONTENT_IN_URL;
+        }
+    }
+
+    @Test
+    public void urlsWithUserDefinedProvider() throws Exception {
+        rule.jenkins.setSecurityRealm(rule.createDummySecurityRealm());
+        User foo = User.getById("foo", true);
+
+        PreferredProviderUserProperty userProperty =
+            new PreferredProviderUserProperty(TestUserDisplayURLProvider.class.getName());
+        foo.addProperty(userProperty);
+        ACL.as(foo);
+
+        MockFolder folder = rule.createFolder("my folder");
+        FreeStyleProject project = (FreeStyleProject) folder
+            .createProject(rule.jenkins.getDescriptorByType(FreeStyleProject.DescriptorImpl.class), "my job",
+                           false);
+        Run<?, ?> run = project.scheduleBuild2(0).get();
+
+        String root = DisplayURLProvider.get().getRoot();
+        assertEquals("http://localhost:" + rule.getLocalPort() + "/jenkins/", root);
+        assertEquals(root + "job/my%20folder/job/my%20job/1/" + TestUserDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getRunURL(run));
+        assertEquals(root + "job/my%20folder/job/my%20job/" + TestUserDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getJobURL(project));
+        assertEquals(root + "job/my%20folder/job/my%20job/1/artifact/" + TestUserDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getArtifactsURL(run));
+        assertEquals(root + "job/my%20folder/job/my%20job/changes/" + TestUserDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getChangesURL(run));
+        assertEquals(root + "job/my%20folder/job/my%20job/1/testReport/" + TestUserDisplayURLProvider.EXTRA_CONTENT_IN_URL,
+                     DisplayURLProvider.get().getTestsURL(run));
+
+        EnvVars environment = run.getEnvironment();
+        assertEquals(DisplayURLProvider.get().getRunURL(run), environment.get("RUN_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getArtifactsURL(run), environment.get("RUN_ARTIFACTS_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getChangesURL(run), environment.get("RUN_CHANGES_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getTestsURL(run), environment.get("RUN_TESTS_DISPLAY_URL"));
+        assertEquals(DisplayURLProvider.get().getJobURL(project), environment.get("JOB_DISPLAY_URL"));
+    }
+
+
     @Test
     public void decoration() throws Exception {
         MockFolder folder = rule.createFolder("my folder");
@@ -71,9 +233,8 @@ public class DisplayURLProviderTest {
         assertEquals(root + "job/my%20folder/job/my%20job/1/display/redirect?page=tests&utm_campaign=jenkins"
                         + "&utm_source=Jenkins&utm_term=my+folder%2Fmy+job%231",
                 DisplayURLProvider.get().getTestsURL(run));
-        DisplayURLContext ctx = DisplayURLContext.open();
-        try {
-            ctx.plugin(Jenkins.getActiveInstance().getPluginManager().getPlugin("display-url-api"));
+        try (DisplayURLContext ctx = DisplayURLContext.open()) {
+            ctx.plugin(Jenkins.get().getPluginManager().getPlugin("display-url-api"));
             assertEquals(
                     root
                             + "job/my%20folder/job/my%20job/1/display/redirect?utm_campaign=display-url-api"
@@ -91,8 +252,6 @@ public class DisplayURLProviderTest {
             assertEquals(root + "job/my%20folder/job/my%20job/1/display/redirect?page=tests&utm_campaign=display"
                             + "-url-api&utm_source=Jenkins&utm_term=my+folder%2Fmy+job%231",
                     DisplayURLProvider.get().getTestsURL(run));
-        } finally {
-            ctx.close();
         }
 
         EnvVars environment = run.getEnvironment();
