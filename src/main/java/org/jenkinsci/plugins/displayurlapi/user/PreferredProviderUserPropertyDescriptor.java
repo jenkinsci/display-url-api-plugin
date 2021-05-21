23223293e872cb5f674a3283cadb5ed2867450f3
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-05-21 10:34:58 +0100

    Fix styling of provider selector (#64)
    * Fix styling of provider selector
    
    * Use correct jelly tag

--- src/main/java/org/jenkinsci/plugins/displayurlapi/user/PreferredProviderUserPropertyDescriptor.java
+++ src/main/java/org/jenkinsci/plugins/displayurlapi/user/PreferredProviderUserPropertyDescriptor.java
@@ -4,6 +4,7 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
+import hudson.util.ListBoxModel;
 import org.jenkinsci.plugins.displayurlapi.Messages;
 import org.jenkinsci.plugins.displayurlapi.user.PreferredProviderUserProperty.ProviderOption;
 
@@ -16,6 +17,20 @@ public class PreferredProviderUserPropertyDescriptor extends UserPropertyDescrip
     @Override
     public UserProperty newInstance(User user) {
         return new PreferredProviderUserProperty(ProviderOption.DEFAULT_OPTION.getId());
+    }
+
+    public ListBoxModel doFillProviderIdItems() {
+        ListBoxModel items = new ListBoxModel();
+        PreferredProviderUserProperty property = PreferredProviderUserProperty.forCurrentUser();
+        for (ProviderOption providerOption : property.getAll()) {
+            ListBoxModel.Option option = new ListBoxModel.Option(
+                    providerOption.getName(),
+                    providerOption.getId(),
+                    property.isSelected(providerOption.getId())
+            );
+            items.add(option);
+        }
+        return items;
     }
 
     @Override
